6b8d31ab6d6b554a3352a85122b1568f75b60215
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-26 07:56:16 +0530

    added a footer component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,61 +1,88 @@
+"use client"
 import { CourseCard } from "../components/course-card";
-import { PreRegForm } from "../components/pre-register-form";
+import { Footer } from "../components/footer";
+
 
 export default function Page(): JSX.Element {
-  const courses = [{
-    title: "Advanced Web3 Cohort",
-    status: "Upcoming",
-    thumbnail: "https://appx-wsb-gcp.akamai.net.in/paid_course3/2023-11-10-0.3523174787735883.jpeg",
-    links: [{
-      name: "Pre Register",
-      url: "#"
-    }, {
-      name: "View Syllabus",
-      url: "#"
-    }]
-  }, {
-    title: "100xDevs Cohort 3",
-    status: "Upcoming",
-    thumbnail: "https://appx-wsb-gcp.akamai.net.in/teachcode/admin/COURSE/cover/1699610005757WhatsApp-Image-2023-11-10-at-3.16.18-PM.jpeg",
-    links: [{
-      name: "Pre Register",
-      url: "#"
-    }, {
-      name: "View Syllabus",
-      url: "#"
-    }]
-  },
-  {
-    title: "Full Stack Cohort",
-    status: "Completed",
-    thumbnail: "https://appx-wsb-gcp.akamai.net.in/teachcode/admin/COURSE/cover/1699610005757WhatsApp-Image-2023-11-10-at-3.16.18-PM.jpeg",
-    links: [{
-      name: "Purchase",
-      url: "https://harkirat.classx.co.in/new-courses/2-live-full-stack-open-source-cohort-1-finished"
-    }, {
-      name: "View Syllabus",
-      url: "https://harkirat.classx.co.in/new-courses/2-live-full-stack-open-source-cohort-1-finished"
-    }
-    ]
-  },
-  {
-    title: "100xDevs Cohort 0-100",
-    status: "OnGoing",
-    thumbnail: "https://appx-wsb-gcp.akamai.net.in/teachcode/admin/COURSE/cover/1699610005757WhatsApp-Image-2023-11-10-at-3.16.18-PM.jpeg",
-    links: [{
-      name: "Purchase Now",
-      url: "https://harkirat.classx.co.in/new-courses"
-    }, {
-      name: "View Syllabus",
-      url: "https://harkirat.classx.co.in/new-courses"
-    }
-    ]
-  }
-  ]
+  const courses = [
+    {
+      title: "Advanced Web3 Cohort",
+      status: "Upcoming",
+      thumbnail:
+        "https://appx-wsb-gcp.akamai.net.in/paid_course3/2023-11-10-0.3523174787735883.jpeg",
+      links: [
+        {
+          name: "Pre Register",
+          url: "#",
+        },
+        {
+          name: "View Syllabus",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "100xDevs Cohort 3",
+      status: "Upcoming",
+      thumbnail:
+        "https://appx-wsb-gcp.akamai.net.in/teachcode/admin/COURSE/cover/1699610005757WhatsApp-Image-2023-11-10-at-3.16.18-PM.jpeg",
+      links: [
+        {
+          name: "Pre Register",
+          url: "#",
+        },
+        {
+          name: "View Syllabus",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Full Stack Cohort",
+      status: "Completed",
+      thumbnail:
+        "https://appx-wsb-gcp.akamai.net.in/teachcode/admin/COURSE/cover/1699610005757WhatsApp-Image-2023-11-10-at-3.16.18-PM.jpeg",
+      links: [
+        {
+          name: "Purchase",
+          url: "https://harkirat.classx.co.in/new-courses/2-live-full-stack-open-source-cohort-1-finished",
+        },
+        {
+          name: "View Syllabus",
+          url: "https://harkirat.classx.co.in/new-courses/2-live-full-stack-open-source-cohort-1-finished",
+        },
+      ],
+    },
+    {
+      title: "100xDevs Cohort 0-100",
+      status: "OnGoing",
+      thumbnail:
+        "https://appx-wsb-gcp.akamai.net.in/teachcode/admin/COURSE/cover/1699610005757WhatsApp-Image-2023-11-10-at-3.16.18-PM.jpeg",
+      links: [
+        {
+          name: "Purchase Now",
+          url: "https://harkirat.classx.co.in/new-courses",
+        },
+        {
+          name: "View Syllabus",
+          url: "https://harkirat.classx.co.in/new-courses",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="bg-white relative flex flex-col items-center justify-center mt-6">
       <section className="w-full p-8 flex flex-col justify-center items-center dark:bg-gray-800">
-        <video width="1250" height="240" controls preload="none" className="rounded-2xl bg-black p-1 mt-[1rem] m-4 drop-shadow-xl mt-10" autoPlay muted>
+        <video
+          width="1250"
+          height="240"
+          controls
+          preload="none"
+          className="rounded-2xl bg-black p-1 m-4 drop-shadow-xl mt-10"
+          autoPlay
+          muted
+        >
           <source src="test.mp4" type="video/mp4" />
           <track
             src={`https://appx-wsb-gcp.akamai.net.in/teachcode/admin/COURSE/cover/1699610005757WhatsApp-Image-2023-11-10-at-3.16.18-PM.jpeg`}
@@ -72,18 +99,32 @@ export default function Page(): JSX.Element {
 
       <section className="w-full p-8 flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-900">
         <div className="grid grid-cols-2 w-screen mt-[3rem]">
-          <h1 className="text-[3rem] text-black font-bold dark:text-white col-span-2 text-center">Upcoming Cohorts</h1>
+          <h1 className="text-[3rem] text-black font-bold dark:text-white col-span-2 text-center">
+            Upcoming Cohorts
+          </h1>
           <div className="flex w-screen justify-evenly py-8 px-4">
-            {courses.map(course => course.status === "Upcoming" ? (<CourseCard course={course} />) : null)}
+            {courses.map((course) =>
+              course.status === "Upcoming" ? (
+                <CourseCard course={course} key={course.title} />
+              ) : null
+            )}
           </div>
         </div>
       </section>
+
       <section className="w-full p-8 flex flex-col justify-center items-center dark:bg-gray-800">
-        <h1 className="text-[3rem] text-black font-bold dark:text-white col-span-2 text-center">Available Cohorts</h1>
+        <h1 className="text-[3rem] text-black font-bold dark:text-white col-span-2 text-center">
+          Available Cohorts
+        </h1>
         <div className="flex w-screen justify-evenly py-8">
-          {courses.map(course => course.status !== "Upcoming" ? (<CourseCard course={course} />) : null)}
+          {courses.map((course) =>
+            course.status !== "Upcoming" ? (
+              <CourseCard course={course} key={course.title} />
+            ) : null
+          )}
         </div>
       </section>
+    <Footer/>
     </div>
   );
 }
--- apps/web/src/components/course-card.tsx
+++ apps/web/src/components/course-card.tsx
@@ -26,7 +26,7 @@ export function CourseCard({ course }: { course: any }) {
             }
             return (
               <Link href={link.url} className="scale-75">
-               <Button className="hover:bg-transparent text-black w-fit h-fit dark:bg-transparent cursor-pointer scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 dark:hover:bg-blue-500 bg-white  hover:bg-blue-400 flex-shrink-0 hover:shadow-lg hover:scale-105 transition duration-200 hover:border hover:border-black dark:hover:border-white">
+               <Button className="hover:bg-transparent text-black w-fit h-fit dark:bg-transparent cursor-pointer scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 dark:hover:bg-blue-500 bg-white  hover:bg-blue-400 flex-shrink-0 hover:scale-105 hover:border hover:border-black dark:hover:border-white">
                      <span className="text-[2rem] dark:text-white">{link.name}</span>
                </Button>
 
